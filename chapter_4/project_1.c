789bcf64a0ad22c206a615d7ce88a7f50cdb2ef1
edh08001 <49404157+edh08001@users.noreply.github.com>
2025-02-08 03:36:08 +0000

    Delete chapter_4 directory

--- chapter_4/project_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  int d;
-  printf("Enter a 2 digit number: ");
-  scanf("%d", &d);
-  printf("The reversal of that number is: %d\n", ((d % 10) * 10 + d / 10));
-}
